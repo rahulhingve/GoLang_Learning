4fa7d8a75d11da0941b980356800ee1bb64f12d7
unknown <rahulpawar2001.rp@gmail.com>
2024-07-22 19:29:58 +0530

    slices  in golang

---
+++ 09slices/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("hello from slice ")
+
+	var moblist = []string{"apple", "sony", "mi"}
+	fmt.Printf("type of %T \n", moblist)
+	moblist = append(moblist, "vivo shit ", "realme shit")
+	fmt.Println(moblist)
+	moblist = append(moblist[1:4]) // values giving in append cant be inclusive like it cannot be shown
+	fmt.Println(moblist)
+
+	score := make([]int, 4)
+
+	score[0] = 11
+	score[1] = 19
+	score[2] = 13
+	score[3] = 14
+
+	score = append(score, 15, 16, 17)
+	fmt.Println(score)
+
+	sort.Ints(score)
+	fmt.Println(score)
+	fmt.Println(sort.IntsAreSorted(score))
+}
